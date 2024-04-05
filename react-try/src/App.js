4bc8c93b0a4408e9d9a41563b4275e8b169ff30e
Horipriya Das Arpita <104673904+Horipriya-Arpita@users.noreply.github.com>
2024-04-06 00:03:24 +0600

    New file created

--- react-try/src/App.js
+++ react-try/src/App.js
@@ -1,12 +1,15 @@
 import Component from "./Components/Component";
 import Buttons from "./Components/Buttons";
 import List from "./Components/List";
+import New from "./Components/New";
 function App() {
   return (
     <div className="App">
       <Component />
       <Buttons />
       <List />
+      <New />
+      
     </div>
   );
 }
